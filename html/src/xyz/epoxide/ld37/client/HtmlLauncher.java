45c6a5e8174c91ed40e7080cea7cb86bc7748ea2
lclc98 <liam.c14785@gmail.com>
2016-12-10 12:03:29 +1000

    Initial Project Setup

---
+++ html/src/xyz/epoxide/ld37/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package xyz.epoxide.ld37.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import xyz.epoxide.ld37.LD37;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new LD37();
+        }
+}
