6a56fe08a95315d5dcd8d7bc3d4ed6bae781ed77
Tyler Hancock <darklime@live.ca>
2016-12-10 00:22:04 -0700

    Changed Tile to use new registry system.

--- core/src/xyz/epoxide/ld37/tile/Tile.java
+++ core/src/xyz/epoxide/ld37/tile/Tile.java
@@ -1,31 +1,44 @@
 package xyz.epoxide.ld37.tile;
 
 import xyz.epoxide.commons.collections.BiMap;
+import xyz.epoxide.commons.registry.IRegisterable.Registerable;
 import xyz.epoxide.commons.registry.Identifier;
+import xyz.epoxide.commons.registry.NamedRegistry;
+import xyz.epoxide.ld37.LD37;
 
-public class Tile {
+public class Tile extends Registerable<Tile> {
+    
     public static BiMap<Tile, Integer> tileColorMap = new BiMap<Tile, Integer>();
-
-    public static final Tile VOID = new Tile(new Identifier("ld37:void"), -1);
-    public static final Tile STONE_PILLAR_BASE = new Tile(new Identifier("ld37:stone"), -16777216).setUV(1.0f / 256.0f, 48.0f / 256.0f, 7.0f / 256.0f, 64.0f / 256.0f);
-    public static final Tile WATER = new Tile(new Identifier("ld37:water"), -4714223);
-
-    public final Identifier id;
+    
+    public static final NamedRegistry<Tile> REGISTRY = new NamedRegistry<Tile>();
+    
+    public static final Tile VOID = registerTile("void", new Tile(-1));
+    public static final Tile STONE_PILLAR_BASE = registerTile("stone", new Tile(-16777216).setUV(1.0f / 256.0f, 48.0f / 256.0f, 7.0f / 256.0f, 64.0f / 256.0f));
+    public static final Tile WATER = registerTile("water", new Tile(-4714223));
+    
     private final int color;
-
+    
     public float u, v, u2, v2;
-
-    public Tile(Identifier id, int color) {
-        this.id = id;
+    
+    public Tile(int color) {
         this.color = color;
         tileColorMap.put(this, color);
     }
-
-    public static Tile getTile(int color) {
+    
+    private static Tile registerTile (String id, Tile tile) {
+        
+        tile.setIdentifier(new Identifier(LD37.ID, id));
+        REGISTRY.registerValue(tile);
+        return tile;
+    }
+    
+    public static Tile getTile (int color) {
+        
         return tileColorMap.getInverse().get(color);
     }
-
-    private Tile setUV(float u, float v, float u2, float v2) {
+    
+    private Tile setUV (float u, float v, float u2, float v2) {
+        
         this.u = u;
         this.v = v;
         this.u2 = u2;
